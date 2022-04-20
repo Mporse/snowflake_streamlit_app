e3560c9b4546206402cd913b52f6d287840995a8
Mads <mads.po.ped@gmail.com>
2022-04-20 10:23:21 +0200

    Add option for user to enter fruit name to get info from fruityvice API

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,10 @@ streamlit.dataframe(fruits_to_show)
 
 # Get Fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input("What fruit would you like information about?", "Kiwi")
+streamlit.write("The user entered", fruit_choice)
 
-fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/" + fruit_choice)
 
 # Normalize the API response
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
