644327216e94d5b80f0341e885aea4e4ca930940
Mads <mads.po.ped@gmail.com>
2022-04-20 08:07:20 +0200

    Update Fruityvice API request

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,15 @@ import requests
 
 streamlit.title("My Parents' New Healthy Diner")
 
-streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header("Breakfast Favorites")
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
 
 
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index("Fruit")
@@ -26,6 +26,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display table of fruit
 streamlit.dataframe(fruits_to_show)
 
-# Get Fruityvice request
+# Get Fruityvice API response
+streamlit.header("Fruityvice Fruit Advice!")
+
 fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
